a48a8c9469602cbe5df6e065f30bebcf74d0fdc1
Thejani Kulathunga <thejanikulathunga10@gmail.com>
2025-05-07 00:57:35 +0530

    Designed Navbar

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -4,28 +4,30 @@ import TabSwitcher from '../Components/TabSwitcher';
 import FeedTab from '../Components/FeedTab';
 import ProgressTab from '../Components/ProgressTab';
 import PlansTab from '../Components/PlansTab';
+import Navbar from '../Components/Navbar';
 
 const HomePage = () => {
   const [activeTab, setActiveTab] = useState('feed');
 
   return (
     <div className="home-wrapper">
-      <nav className="nav-bar">
+      <Navbar />
+      {/* <nav className="nav-bar">
         <h2 className="logo">CodeShare</h2>
         <input className="search-input" type="text" placeholder="Search developers, skills, topics..." />
         <div className="nav-icons">
           <button className="icon-btn">🏠</button>
           <button className="icon-btn">🔔</button>
-          <button className="icon-btn">📩</button>
-          <button className="text-xl p-2 rounded-full bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition">
+          <button className="icon-btn">📩</button> */}
+          {/* <button className="text-xl p-2 rounded-full bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition">
             test button 📩
-          </button>
+          </button> */}
 
-          <button className="profile-pic">
+          {/* <button className="profile-pic">
             <img src="https://randomuser.me/api/portraits/men/75.jpg" alt="My Profile" className="profile-thumb small" />
           </button>
         </div>
-      </nav>
+      </nav> */}
 
       <div className="main-content">
         <div className="left-section">
