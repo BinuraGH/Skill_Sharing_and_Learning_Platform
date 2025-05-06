242a27b5bd8e1995ad460db08c7b592c943ae0d6
BinuraGH <binuramenath617@gmail.com>
2025-05-06 14:19:50 +0530

    edited plancrad.jsx and coursedetailsed page

--- frontend/src/Pages/CourseDetailPage.jsx
+++ frontend/src/Pages/CourseDetailPage.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import '../styles/CourseDetailPage.css';
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import "../styles/CourseDetailPage.css";
 
 const CourseDetailPage = () => {
   const { id } = useParams();
